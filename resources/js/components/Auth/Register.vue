dffd5665ead49ff74d00b200b72d26118910fa0b
ianpenalver <jabezpenalver2k20@gmail.com>
2023-10-08 23:59:01 +0800

    Fixed vite and design issue

--- resources/js/components/Auth/Register.vue
+++ resources/js/components/Auth/Register.vue
@@ -2,7 +2,7 @@
     <TopNav />
 
     <main>
-        <div class="container">
+        <div class="container mt-5">
 
             <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
                 <div class="container">
