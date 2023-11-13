003148657e2ab01ee9e0754f0277e40c3a9aa94c
ianpenalver <jabezpenalver2k20@gmail.com>
2023-11-13 13:05:51 +0800

    Changed title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Appetiser Todo App</title>
+    <title> Todo App</title>
 
     <link href="{{ asset('assets/images/favicon.png') }}" rel="icon">
     <link href="{{ asset('assets/images/apple-touch-icon.png') }}" rel="apple-touch-icon">
